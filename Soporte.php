f5dd3c3eb53dd2b5e7297a4da7e65bd60cec45a8
Jaime Naharro <jaimenaharro5@gmail.com>
2024-10-28 14:07:45 +0100

    inicializando proyecto

--- Soporte.php
+++ Soporte.php
@@ -1,3 +1,31 @@
 <?php
-    
+declare(strict_types=1);
+    class Soporte{
+        const IVA=0.21;
+        public function __construct(
+            public string $titulo,
+            protected int $numero,
+            private float $precio
+            
+            ){}
+            
+
+            
+            public function getPrecio():float
+            {
+                return $this->precio;
+            }
+            public function getPrecioConIva():float
+            {
+                return $this->getPrecio()+$this->getPrecio();
+            }
+            public function getNumero():int
+            {
+                return $this->numero;
+            }
+            public function muestraResumen():string
+            {
+                return "";
+            }
+    }
 ?>
